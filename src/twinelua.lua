072e9e621df407b2aabf75be70b02e2dc6d6f1d1
bensku <bmi@mbnet.fi>
2015-07-23 18:55:13 +0300

    First commit

---
+++ src/twinelua.lua
@@ -0,0 +1,13 @@
+-- Importer for Twine's "twee" format.
+
+local twinelua = {}
+
+--- Import data from string.
+-- @param #string text: Twee data as string
+-- @return #object: Imported data structure in Lua, or nil in case of fail
+-- @return #string: If import failed, error message
+function twinelua:importData(text)
+  
+end
+
+return twinelua
